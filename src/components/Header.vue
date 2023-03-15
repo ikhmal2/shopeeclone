978df6eed3e52fb9d38e2ad013652fb0df152b39
ikhmal2 <ax.ikhmal.145713@gmail.com>
2023-03-16 00:30:21 +0800

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="bg-amber-500">
+        <nav class="flex justify-between p-5">
+            <ul class="flex">
+                <li>Seller Centre | </li>
+                <li>Download | </li>
+                <li>Follow us on </li>
+            </ul>
+            <ul class="flex">
+                <li>Notifications</li>
+                <li>Help</li>
+                <li>English</li>
+                <li>Sign UP | Login</li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>
