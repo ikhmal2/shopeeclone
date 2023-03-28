b891a5af4ce4c2a6b3300789aa65809f01c99cd7
ikhmal2 <ax.ikhmal.145713@gmail.com>
2023-03-28 17:30:07 +0800

    added custom colors, and jit mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
   content: ["./src/**/*.{html,js,vue}"],
   theme: {
     extend: {
       colors: {
-        orange: "#eda500",
+        primary: "#f8482f",
+        secondary: "#ff7337",
+        text: "#333",
+        secondary_text: "#808080",
       },
     },
   },
