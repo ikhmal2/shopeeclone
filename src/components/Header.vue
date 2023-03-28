4c5b9ddb9130838a9a5bc107ae03563c9531670e
ikhmal2 <ax.ikhmal.145713@gmail.com>
2023-03-28 17:30:37 +0800

    combined header and searchbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,26 @@
+<script>
+import SearchBar from './searchbar.vue';
+export default {
+    components: {
+        SearchBar
+    }
+}
+</script>
+
 <template>
-    <header class="flex justify-between p-5 bg-orange">
-        <div>
-            shopeeC
-        </div>
-        <div class="flex">
-            <div class="px-1">Login/Sign Up</div>
-            <div class="px-1">Cart</div>
+    <header class="flex justify-between p-5 bg-gradient-to-b from-primary to-secondary">
+        <h1 class="text-2xl font-bold text-white">
+            SHOPEE<span>C</span>
+        </h1>
+        <SearchBar />
+        <div class="flex text-white">
+            <div class="px-1 cursor-pointer hover:text-secondary_text">Login/Sign Up</div>
+            <div class="px-1 cursor-pointer hover:text-secondary_text">Cart</div>
         </div>
     </header>
 </template>
 
-<script setup>
 
-</script>
+
 
 <style scoped></style>
