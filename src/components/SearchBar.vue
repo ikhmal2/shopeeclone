c9518ba7f38aaffe2f4579871d1d1d48cf1fe80b
ikhmal2 <ax.ikhmal.145713@gmail.com>
2023-03-28 17:30:49 +0800

    created structure

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <div class="shadow-xl w-3/5">
+        <div class="shopee-searchbar__main">
+            <form role="search" class="shopee-searchbar-input" autocomplete="off"><input
+                    aria-label="Sign Up &amp; Get RM18 Off Voucher!" class="shopee-searchbar-input__input" maxlength="128"
+                    placeholder="Sign Up &amp; Get RM18 Off Voucher!" autocomplete="off" aria-autocomplete="list"
+                    aria-controls="shopee-searchbar-listbox" aria-expanded="false" role="combobox" value=""></form>
+        </div><button type="button" class="btn btn-solid-primary btn--s btn--inline shopee-searchbar__search-button"><svg
+                height="19" viewBox="0 0 19 19" width="19" class="shopee-svg-icon">
+                <g fill-rule="evenodd" stroke="none" stroke-width="1">
+                    <g transform="translate(-1016 -32)">
+                        <g>
+                            <g transform="translate(405 21)">
+                                <g transform="translate(611 11)">
+                                    <path
+                                        d="m8 16c4.418278 0 8-3.581722 8-8s-3.581722-8-8-8-8 3.581722-8 8 3.581722 8 8 8zm0-2c-3.3137085 0-6-2.6862915-6-6s2.6862915-6 6-6 6 2.6862915 6 6-2.6862915 6-6 6z">
+                                    </path>
+                                    <path
+                                        d="m12.2972351 13.7114222 4.9799555 4.919354c.3929077.3881263 1.0260608.3842503 1.4141871-.0086574.3881263-.3929076.3842503-1.0260607-.0086574-1.414187l-4.9799554-4.919354c-.3929077-.3881263-1.0260608-.3842503-1.4141871.0086573-.3881263.3929077-.3842503 1.0260608.0086573 1.4141871z">
+                                    </path>
+                                </g>
+                            </g>
+                        </g>
+                    </g>
+                </g>
+            </svg></button>
+    </div>
+</template>
+
+
+<style scoped></style>
