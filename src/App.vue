5c93a51a62c9d1af8c25a141315360533f146d90
ikhmal2 <ax.ikhmal.145713@gmail.com>
2023-03-30 23:32:47 +0800

    added discover component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Header from './components/Header.vue';
 import Slider from './components/Slider.vue';
+import Discover from './components/Discover.vue';
 </script>
 
 <template>
     <Header />
     <Slider />
+    <Discover />
 </template>
 
 <style scoped></style>
