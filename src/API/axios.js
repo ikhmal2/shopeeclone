ebc625f4a158ea7c75b8f167127101c7803d658c
ikhmal2 <ax.ikhmal.145713@gmail.com>
2023-03-30 23:33:06 +0800

    not needed

--- src/API/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-const storeApi = "https://fakestoreapi.com/";
-
-axios
-  .get(`${storeApi}/products`)
-  .then((res) => res.json())
-  .then((json) => console.log(json));
