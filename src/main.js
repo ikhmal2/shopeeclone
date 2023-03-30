f789606e36c28e742ea35b4f6fbe5dd5e6a6715e
ikhmal2 <ax.ikhmal.145713@gmail.com>
2023-03-30 23:32:53 +0800

    set up vuex

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
+import store from "./store/store.js";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+// createApp(App).mount("#app");
+// createApp(App).use(store);
+
+app.mount("#app");
+app.use(store);
