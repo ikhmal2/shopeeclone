0af5ea9f7fea4ce0a8a0da96fc2cdd17f49f6098
ikhmal2 <ax.ikhmal.145713@gmail.com>
2023-03-30 23:33:39 +0800

    set up vuex

--- src/store/store.js
+++ src/store/store.js
@@ -1,23 +1,25 @@
-import { createApp } from "vue";
 import { createStore } from "vuex";
+import axios from "axios";
 
-// Create a new store instance.
-const store = createStore({
-  state() {
-    return {
-      count: 0,
-    };
+export default createStore({
+  state: {
+    productList: [],
   },
   mutations: {
-    increment(state) {
-      state.count++;
+    addProduct(state, newProduct) {
+      state.productList.push(newProduct);
+    },
+  },
+  actions: {
+    fetchProducts({ commit }) {
+      axios("https://fakestoreapi.com/products").then((response) => {
+        commit("addProduct", response.data);
+      });
+    },
+  },
+  getters: {
+    getProducts(state) {
+      return state.productList;
     },
   },
 });
-
-const app = createApp({
-  /* your root component */
-});
-
-// Install the store instance as a plugin
-app.use(store);
