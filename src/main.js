527acb8eb54f9cbc371925d88340fb781b96ebab
ikhmal2 <ax.ikhmal.145713@gmail.com>
2023-04-03 22:17:56 +0800

    change struct

--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
-import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
-import store from "./store/store.js";
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import store from './store/store.js';
 
-const app = createApp(App);
-// createApp(App).mount("#app");
-// createApp(App).use(store);
-
-app.mount("#app");
-app.use(store);
+createApp(App).use(store).mount('#app');
