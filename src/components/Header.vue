99535e536ae7e259da5e686fa95f3494aceb5622
ikhmal2 <ax.ikhmal.145713@gmail.com>
2023-04-20 23:37:30 +0800

    improving cart

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,12 @@ import SearchBar from './SearchBar.vue';
 export default {
     components: {
         SearchBar
-    }
+    },
+    computed: {
+        cartItems() {
+            return this.$store.getters.getTotalCart;
+        }
+    },
 }
 </script>
 
@@ -15,14 +20,18 @@ export default {
         <SearchBar />
         <div class="flex text-white self-center">
             <div class="px-1 cursor-pointer hover:bg-secondary_text">Login/Sign Up</div>
-            <div class="px-1 cursor-pointer hover:bg-secondary_text">
-                <svg viewBox="0 0 26.6 25.6" class="fill-white w-7 h-7 text-white stroke-white">
-                    <polyline fill="none" points="2 1.7 5.5 1.7 9.6 18.3 21.2 18.3 24.6 6.1 7 6.1" stroke-linecap="round"
-                        stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2.5"></polyline>
-                    <circle cx="10.7" cy="23" r="2.2" stroke="none"></circle>
-                    <circle cx="19.7" cy="23" r="2.2" stroke="none"></circle>
-                </svg>
-            </div>
+            <li class="font-sans block mt-4 lg:inline-block lg:mt-0 align-middle text-black hover:text-gray-700">
+                <a href="#" role="button" class="relative flex">
+                    <svg class="flex-1 w-8 h-8 fill-white" viewbox="0 0 24 24">
+                        <path
+                            d="M17,18C15.89,18 15,18.89 15,20A2,2 0 0,0 17,22A2,2 0 0,0 19,20C19,18.89 18.1,18 17,18M1,2V4H3L6.6,11.59L5.24,14.04C5.09,14.32 5,14.65 5,15A2,2 0 0,0 7,17H19V15H7.42A0.25,0.25 0 0,1 7.17,14.75C7.17,14.7 7.18,14.66 7.2,14.63L8.1,13H15.55C16.3,13 16.96,12.58 17.3,11.97L20.88,5.5C20.95,5.34 21,5.17 21,5A1,1 0 0,0 20,4H5.21L4.27,2M7,18C5.89,18 5,18.89 5,20A2,2 0 0,0 7,22A2,2 0 0,0 9,20C9,18.89 8.1,18 7,18Z" />
+                    </svg>
+                    <span
+                        class="absolute right-0 top-0 rounded-full bg-red-600 w-4 h-4 top right p-0 m-0 text-white font-mono text-sm  leading-tight text-center">
+                        {{ cartItems }}
+                    </span>
+                </a>
+            </li>
         </div>
     </header>
 </template>
